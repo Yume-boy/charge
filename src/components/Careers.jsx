2b804403326600dbb9050b4115c846d2e37f2a20
marvellous <comlaf2001@gmail.com>
2024-03-31 16:12:53 +0100

    lol

--- src/components/Careers.jsx
+++ src/components/Careers.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Careers = () => {
   return (
     <div>
-      
+        <div className='rip'>
+        <div className='career-1'>
+          <img src="https://s3-alpha-sig.figma.com/img/7ee8/5f70/6227aa1931287f70eff993c9939fce09?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K9AoIDWw5l4YPfw04Bt7SEzHdIVP0HQxGlGflHxEMxBbMIBp6aZOFZDTLlT~E9oIYJvsYdp~2806Yt3A6IdqJa0VCvpqiXzLwQbvjGs8rv3pIVOLf6EErmzVkw3o-v4kf2CU9Ah5bItKWxzW4M53m549jfoM-HW2TSGVguAnuS3Yw8jvka7wVbo2KE4kPRyLNNJSNdSM0Aecm1i2j6V2sz6FCZhPE4HEep~fkWa0OQ2d-CC6VT2-hEOAAAKJrjtj17B-lLzIV-5e7KsiOtHS-ko7cBbOpi9gFaQQIgi~ZCFcCCMQXucJTRvLtpmF3BNZd8XskHYVDTEtTcLVSFrWAA__" alt="" />
+        </div>
+        <h2>Join our Fast <br /> Growing Team</h2>
+        </div>
     </div>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
         <div className='d-flex top'>
         <div className='links d-flex justify-content-between px-2'>
             <Link to='/About'>About Us</Link>
-            <Link to='/About'>How It Works</Link>
+            <Link to='/Career'>How It Works</Link>
             <Link to='/About'>FAQs</Link>
         </div>
         <div className='links-2 d-flex justify-content-between px-2'>
